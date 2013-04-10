ed6f13ead878a1b71356459c6be6ddd1cdf8fd2c
patricksnape <pts08@ic.ac.uk>
2013-04-10 09:51:28 +0100

    Add make links script for creating hard links Only for Chrome so far

---
+++ make_links.sh
@@ -0,0 +1,8 @@
+rm -f Chrome/libs/*.js
+rm -f Chrome/modules/*.js
+rm -f Chrome/reddit_enhancement_suite.user.js
+
+ln lib/*.js Chrome/libs/
+ln modules/*.js Chrome/modules/
+ln reddit_enhancement_suite.user.js Chrome/
+
