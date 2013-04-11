29d4b1778b93054e6c020be95c99f8af750d9209
patricksnape <pts08@ic.ac.uk>
2013-04-11 10:49:56 +0100

    Add making folders if they don't exist

--- make_links.sh
+++ make_links.sh
@@ -1,7 +1,12 @@
+# CHROME
+#Make dirs if they don't exist (first checkout)
+mkdir -p Chrome/modules
+mkdir -p Chrome/libs
+# Cleanup dead links
 rm -f Chrome/libs/*.js
 rm -f Chrome/modules/*.js
 rm -f Chrome/reddit_enhancement_suite.user.js
-
+# Create new links
 ln lib/*.js Chrome/libs/
 ln modules/*.js Chrome/modules/
 ln reddit_enhancement_suite.user.js Chrome/
